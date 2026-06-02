67d6f604e00eb177193735ee0e270ad4d96656b5
V-Srivatsan <vatsan.programming@gmail.com>
2026-06-02 17:08:32 +0530

    Implemented Force Sync

--- lib/screens/splash/index.dart
+++ lib/screens/splash/index.dart
@@ -9,45 +9,46 @@ import 'package:vstop/lib/fcm.dart';
 import 'package:vstop/screens/login/index.dart' as auth;
 import 'package:vstop/screens/app/index.dart' as app;
 
+const FORCE_SYNC = true;
+
 class Screen extends StatelessWidget {
   const Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
     NotificationService().initializeNotificationSystem();
-
-    () async {
+    PackageInfo.fromPlatform().then((info) async {
       try {
         final latest = jsonDecode((await http.get(
             Uri.parse("https://api.github.com/repos/V-Srivatsan/VStop/releases/latest")
         )).body)['tag_name'];
-        final info = await PackageInfo.fromPlatform();
 
         if (latest != 'v${info.version}' && context.mounted)
           await showDialog(
-            context: context, barrierDismissible: false,
-            builder: (ctx) => AlertDialog(
-              title: Text("Update Available"),
-              content: Text("A new version of V-Stop is available for download!\n\nPlease update to the latest version for the best experience."),
-              actions: [
-                OutlinedButton(onPressed: () => Navigator.pop(ctx), child: Text("Continue")),
-                FilledButton(
-                  onPressed: () => launchUrl(Uri.parse("https://github.com/V-Srivatsan/VStop/releases/latest")),
-                  child: Text("Download")
-                )
-              ]
-            )
+              context: context, barrierDismissible: false,
+              builder: (ctx) => AlertDialog(
+                  title: Text("Update Available"),
+                  content: Text("A new version of V-Stop is available for download!\n\nPlease update to the latest version for the best experience."),
+                  actions: [
+                    OutlinedButton(onPressed: () => Navigator.pop(ctx), child: Text("Continue")),
+                    FilledButton(
+                        onPressed: () => launchUrl(Uri.parse("https://github.com/V-Srivatsan/VStop/releases/latest")),
+                        child: Text("Download")
+                    )
+                  ]
+              )
           );
 
       } catch (e) { print(e); }
       finally {
         final logged = (await SecureStorage.get("username")) != null;
+        final syncVer = await SecureStorage.get("syncVersion");
         if (context.mounted)
           Navigator.pushReplacement(context, MaterialPageRoute(builder: (ctx) =>
-            logged ? app.Screen() : auth.Screen()
+          logged && (FORCE_SYNC ? syncVer != null && info.version == syncVer : true) ? app.Screen() : auth.Screen()
           ));
       }
-    }();
+    });
 
     return Scaffold(
       body: Center(child: Column(
