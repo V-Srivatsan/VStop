02a7a2a7cb41ab86507a75eb08b39f1f791e46cd
V-Srivatsan <vatsan.programming@gmail.com>
2026-06-07 20:27:44 +0530

    Added fallback init for isolates

--- lib/lib/data/index.dart
+++ lib/lib/data/index.dart
@@ -1,4 +1,5 @@
 import 'package:vstop/objectbox.g.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'sem.dart' show SemStore;
 import 'assignments.dart' show AssignmentStore;
@@ -10,7 +11,15 @@ import 'marks.dart' show MarkStore;
 class Database {
   static late Store _store;
 
-  static Future<void> init() async => _store = await openStore();
+  static Future<void> init() async {
+    try { _store = await openStore(); }
+    catch (e) {
+      final dir = await getApplicationDocumentsDirectory();
+      _store = Store.attach(getObjectBoxModel(), '${dir.path}/objectbox');
+    }
+  }
+  static void close() => _store.close();
+
   static void clear([bool full = true]) {
     if (full) { CourseStore.clear(); AssignmentStore.clear(); }
     SemStore.clear(); AcademicCalendar.clear();
